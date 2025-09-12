edf0bc667407f1307c623dd3271f28506df6b972
ConnorN <connor.needham2015@gmail.com>
2025-09-12 13:24:05 -0400

    chore: simplify colcon build script

--- build.sh
+++ build.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
-# Find all files tracked by git that are not part of a submodule
-files=$(git ls-files -- ':!:(*/)' ':!:(*/**/*)' ':!:(*/**/*/*)')
+# Python format
+black . --exclude "src/third-party/|build|install|\.tox|dist"
 
-# Filter out python files into one variable
-python_files=$(
-  for file in $files; do
-    if [[ $file == *.py ]]; then
-      echo "$file"
-    fi
-  done
-)
-
-# Launch one instance of `black` for all python files
-IFS=$'\n' black $python_files
-
-# Format C/C++ source & header files
-for file in $files; do
-  if [[ $file == *.cpp || $file == *.hpp || $file == *.c || $file == *.h ]]; then
-    clang-format -i "$file"
-  fi
-done
+# C++ format
+find ./src -path ./src/third-party -prune -o \
+    \( -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) -print \
+  | xargs clang-format -i --Werror
 
 rosdep install --from-paths src -i -r -y
-
-aarch=$(uname -m)
 
 colcon build --symlink-install --continue-on-error --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc)
