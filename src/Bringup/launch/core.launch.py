2bd87009d45cbe202ce7f6c219347bba0df2d5c0
ConnorN <connor.needham2015@gmail.com>
2025-10-07 11:20:05 -0400

    feat: add bringup support for container launcher

--- src/Bringup/launch/core.launch.py
+++ src/Bringup/launch/core.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 import os
 
@@ -10,16 +11,22 @@ def get_included_launch_descriptions(launch_files):
     for pkg, file in launch_files:
         pkg_share = FindPackageShare(pkg).find(pkg)
         file_path = os.path.join(pkg_share, "launch", file)
-        included_launches.append(
-            IncludeLaunchDescription(PythonLaunchDescriptionSource(file_path))
-        )
+
+        if file.endswith(".py"):
+            source = PythonLaunchDescriptionSource(file_path)
+        elif file.endswith(".xml"):
+            source = XMLLaunchDescriptionSource(file_path)
+        else:
+            raise ValueError(f"Unsupported launch file format: {file}")
+
+        included_launches.append(IncludeLaunchDescription(source))
     return included_launches
 
 
 def generate_launch_description():
-    SetEnvironmentVariable("ROS_LOG_LEVEL", "WARN")
     launch_files = [
         ("joystick_control", "controller.launch.py"),
         ("servo_pkg", "usb_servo_launch.launch.py"),
+        ("rosbridge_server", "rosbridge_websocket_launch.xml"),
     ]
     return LaunchDescription(get_included_launch_descriptions(launch_files))
