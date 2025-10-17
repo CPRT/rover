3b748e4e7caa7c32b36d80be087b2586143e7569
Seysha <PSPuttaguna@gmail.com>
2025-10-17 04:15:17 +0000

    Optimized running speed of Pid_Grapher

--- src/pid_grapher/pid_grapher/base_pid_grapher.py
+++ src/pid_grapher/pid_grapher/base_pid_grapher.py
@@ -1,3 +1,4 @@
+import math
 import threading
 from ros_phoenix.msg import MotorStatus
 import rclpy
@@ -5,12 +6,20 @@ import rclpy.logging
 from rclpy.node import Node
 import rclpy.time as time
 import matplotlib
+import time as pytime
 
 matplotlib.use("TkAgg")  # make sure we have an interactive backend
 
 import matplotlib.pyplot as plt
 from sensor_msgs.msg import Joy
 from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
+from ament_index_python.packages import get_package_share_directory
+import os
+
+plt.rcParams["toolbar"] = "none"
+
+
 """
 For running this, there are some things you may want to note:
     - You have to start the Joy node with ros2 run joy joy_node
@@ -20,10 +29,64 @@ For running this, there are some things you may want to note:
 # NOTE PLEASE REFACTOR WITH WHAT CONNOR SAID (YOU ARE GOING TO GET LISTS AND STUFF)
 
 
+class JointData:
+    def __init__(self, name, ax):
+        self.name = name
+        self.number = int(name.split("_")[1]) - 1
+        self.time = 0
+        self.targetVelocity = 0
+        self.velocity = 0
+        self.times = []
+        self.velocities = []
+        self.targetVelocitiesHistory = []
+        self.targetVelocity = 0
+        self.ax = ax
+        self.ax.set_xlabel("Time (s)")
+        self.ax.set_ylabel("Velocity (rad/s)")
+        self.ax.set_title(name)
+        self.ax.legend(["Velocity", "Target Velocity"])
+        self.ax.grid()
+        self.line1 = self.ax.plot([], [], "-", label="Velocity")[0]
+        self.line2 = self.ax.plot([], [], "--", label="Target")[0]
+        
+        seconds = 30  # Number of seconds to display on the graph
+        
+        self.MAX_POINTS = seconds*10  # Maximum number of points to display on the graph
+
+        self.ax.legend()
+
+        #self.fig.show()
+
+    def plotGraph(self):
+        self.targetVelocitiesHistory.append(self.targetVelocity)
+        self.times.append(self.time)
+        self.velocities.append(self.velocity)
+        
+        if len(self.times) > self.MAX_POINTS:
+            self.times.pop(0)
+            self.velocities.pop(0)
+            self.targetVelocitiesHistory.pop(0)
+
+        self.line1.set_xdata(self.times)
+        self.line2.set_xdata(self.times)
+
+        self.line1.set_ydata(self.velocities)
+        self.line2.set_ydata(self.targetVelocitiesHistory)
+
+        self.ax.relim()
+        self.ax.autoscale_view()
+
 class BasePIDGrapher(Node):
-    initiated = False
+    
+    def update_plots(self):
+        rate = 10
+        period = 1.0 / rate
+        for j in self.Joints.values():
+            j.plotGraph()
+        self.fig.canvas.draw_idle()  # schedules one redraw for all 6 subplots
+        plt.pause(0.000000001)
+            
     def __init__(self):
-        self.joystickVelocity = 0.0
         super().__init__("base_pid_grapher")
         self.subscription = self.create_subscription(
             JointState,
@@ -31,89 +94,88 @@ class BasePIDGrapher(Node):
             self.plotter_callback,
             10,
         )
-        self.subscription = self.create_subscription(
-            Joy, "/joy", self.joystick_callback, 10
+        self.subscription_targets = self.create_subscription(
+            JointTrajectory,
+            "/rover_arm_controller/joint_trajectory",
+            self.trajectory_callback,
+            10,
         )
 
-        print("TEST - Maybe found callback? Also running node")
         self.targetVelocity = 10
 
         plt.ion()
-
-        plt.rcParams["toolbar"] = "none"
-
-        self.names = []
-        self.figs = []
-        self.axs = []
-        self.velocities = [[],[],[],[],[],[]]
-        self.times = [[],[],[],[],[],[]]
-        self.targetVelocities = [[],[],[],[],[],[]]
-        self.line1s = []#TODO make this actually work im too fucking lazy right now
-        self.line2s = []
         
-        for i in range(0, len(self.times)):
-            fig, ax = plt.subplots()
-            self.figs.append(fig)
-            self.axs.append(ax)
+        joint_names = ["Joint_1", "Joint_2", "Joint_3", "Joint_4", "Joint_5", "Joint_6"]
 
         
-        for i in range(0,len(self.times)):
-            self.line1s.append(self.ax.plot(self.times[i], self.velocities[i], "b-"))
-            self.line2s.append(self.ax.plot(self.times[i], self.targetVelocities[i], "b-"))
+        n_plots = len(joint_names)
+        cols = 2
+        rows = math.ceil(n_plots / cols)
 
-        (self.line1,) = self.ax.plot(self.x_data, self.y_data, "b-")
-        (self.line2,) = self.ax.plot(self.x_data2, self.y_data2, "b-")
+        self.fig, self.axs = plt.subplots(rows, cols, figsize=(12, rows*3))
+        
+        #self.fig, self.axs = plt.subplots(3, 2, figsize=(12, 8))  # 3 rows × 2 columns
+        self.fig.subplots_adjust(left=0.07, right=0.95, top=0.95, bottom=0.07, hspace=0.3, wspace=0.3)
+        self.axs = self.axs.flatten()  # make it easy to index as a list
+        self.Joints = {}
+        for name, ax in zip(joint_names, self.axs):
+            self.Joints[name] = JointData(name, ax)
+        
+        plt.show(block=False)
 
-        self.fig.show()
 
+        self.timer = self.create_timer(0.1, self.update_plots)
+            
         self.subscription
 
         self.beginning = self.get_clock().now()
-
-    def plotGraph(self, x, y, setpoint,motor_id):
-        print("Graphing")
-        self.times[motor_id].append(x)
-        self.velocities[motor_id].append(y)
-        self.targetVelocities[motor_id].append(setpoint)
-
-        self.line1s[motor_id].set_xdata(self.times[motor_id])
-        self.line2s[motor_id].set_xdata(self.times[motor_id])
-
-        self.line1s[motor_id].set_ydata(self.velocities[motor_id])
-        self.line2s[motor_id].set_ydata(self.targetVelocities[motor_id])
-
-        self.axs[motor_id].relim()
-        self.axs[motor_id].autoscale_view()
-        self.figs[motor_id].canvas.draw()
-        self.figs[motor_id].canvas.flush_events()
-
-        plt.pause(0.001)
-
-    def plotter_callback(self, msg):
-        self.get_logger().info(f"Velocity recieved: {msg.velocities[0]}")
-        now = self.get_clock().now()
         
-        if (self.initiated == False):
-            self.names = msg.names
-            for i in msg.names:
-                self.axs[i].set_title(i)
+        self.fig.canvas.mpl_connect("key_press_event", self.on_key)
+                
+    def plotter_callback(self, msg):
+        now = self.get_clock().now()
 
-        print("There is something running in the callback")
         delta = now - self.beginning
         x = delta.nanoseconds / 1e9
-        for i in range(0,len(msg.names)):
-            self.plotGraph(x, float(msg.velocities[i]), self.targetVelocities[i],i)
+        for i in range(0, len(msg.name)):
+            if msg.name[i] in self.Joints:
+                Joint = self.Joints[msg.name[i]]
+                Joint.time = x
+                Joint.velocity = msg.velocity[i]
 
-    def joystick_callback(self, stick):
-        self.joystickVelocity = stick.axes[1]
-        print(self.joystickVelocity)
 
     def _show_plot(self):
         plt.show(block=True)
 
+    def trajectory_callback(self, msg):
+        if len(msg.points) > 0:
+            point = msg.points[-1]  # Get the last point in the trajectory
+            for i in range(0, len(msg.joint_names)):
+                jointName = msg.joint_names[i]
+                self.Joints[jointName].targetVelocity = point.velocities[i]
+    
+    def on_key(self, event):
+        if event.key == "s":
+            self.save()
+            print("Saved plot")
+    
+    def save(self):
+        # OR force the src directory like this:
+        script_dir = os.path.join(os.getcwd(), 'pid_grapher')
+        plots_dir = os.path.join(script_dir, "plots")
+        os.makedirs(plots_dir, exist_ok=True)
+
+        save_path = os.path.join(plots_dir, "joint_plot.png")
+        plt.savefig(save_path, bbox_inches='tight', dpi=300)
+        print(f"Plot saved to {save_path}")
+    """
+    TODO this mapping is very fucked but I don't have it in me right now so ill figure this logic out later
+    perhaps a dictionary? Oh wait, that's actually not a bad idea but I would have to init everything after the names come in?
+    Fuck it, that's a good idea, sounds like a future me problem, I have midterms to study for
+    """
+
 
 def main(args=None):
-    print("TEST - Running Main")
     rclpy.init(args=args)
     node = BasePIDGrapher()
     rclpy.spin(node)
