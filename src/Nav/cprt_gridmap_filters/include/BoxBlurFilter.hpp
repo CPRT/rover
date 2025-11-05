561fc9ddfcbdcf8e76cddb7813d32357383f0855
Lauren Spargo <lauren.e.spargo@gmail.com>
2025-11-05 04:02:29 +0000

    ETH Zurich meanInRadiusFilter

---
+++ src/Nav/cprt_gridmap_filters/include/BoxBlurFilter.hpp
@@ -0,0 +1,62 @@
+/**
+ * PreserveCostInflationFilter.hpp
+ *
+ *    Created on: Oct 4th, 2025
+ *        Author: Lauren Spargo
+ *  Organization: Carleton Planetary Robotics Team
+ */
+
+#ifndef CPRTGRIDMAPFILTERS_BOXBLURFILTER_HPP_
+#define CPRTGRIDMAPFILTERS_BOXBLURFILTER_HPP_
+#include <string>
+
+#include <filters/filter_base.hpp>
+
+#include <string>
+#include <vector>
+
+namespace grid_map {
+
+/*!
+ * Filter class to find the mean of the values inside a radius.
+ */
+template <typename T> class BoxBlurFilter : public filters::FilterBase<T> {
+public:
+  /*!
+   * Constructor
+   */
+  BoxBlurFilter();
+
+  /*!
+   * Destructor.
+   */
+  virtual ~BoxBlurFilter();
+
+  /*!
+   * Configures the filter from parameters on the Parameter Server
+   */
+  bool configure() override;
+
+  /*!
+   * Computes for each value in the input layer the mean of all values in a
+   * radius around it Saves this mean in an additional output layer.
+   * @param mapIn grid map containing the input layer.
+   * @param mapOut grid map containing the layers of the input map and the new
+   * layer.
+   */
+  bool update(const T &mapIn, T &mapOut) override;
+
+private:
+  //! Radius to take the mean from.
+  double radius_;
+
+  //! Input layer name.
+  std::string inputLayer_;
+
+  //! Output layer name.
+  std::string outputLayer_;
+};
+
+} // namespace grid_map
+
+#endif // CPRTGRIDMAPFILTERS_PRESERVECOSTINFLATIONFILTER_HPP_
