f8179f52f2da5fbdf7b7b4b28c2bd354613c51fd
Connor Needham <129120300+ConnorNeed@users.noreply.github.com>
2025-11-07 16:32:12 -0400

    Nav launch (#21)
    * fix: navigation launch
    
    * chore: update readme
    
    * chore: add convenience script to source ros env
    
    * chore: add zed folder to volumes for dev container
    
    * chore: format
    
    * chore: add gridmap filter dependancy
    
    * chore: add host zed SDK to setup package

--- src/Nav/navigation/launch/navigation.launch.py
+++ src/Nav/navigation/launch/navigation.launch.py
@@ -16,17 +16,17 @@ def generate_launch_description():
     # Launch configurations
     use_sim_time = LaunchConfiguration("use_sim_time")
     svo_path = LaunchConfiguration("svo_path")
-    launch_transversability = LaunchConfiguration("launch_transversability")
+    launch_traversability = LaunchConfiguration("launch_traversability")
     launch_zed = LaunchConfiguration("launch_zed")
     launch_ouster = LaunchConfiguration("launch_ouster")
 
     # Declare launch arguments
     declare_use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false")
-    declare_transversability = DeclareLaunchArgument(
-        "launch_transversability", default_value="True"
+    declare_traversability = DeclareLaunchArgument(
+        "launch_traversability", default_value="True"
     )
     declare_zed = DeclareLaunchArgument("launch_zed", default_value="True")
-    declare_ouster = DeclareLaunchArgument("launch_ouster", default_value="True")
+    declare_ouster = DeclareLaunchArgument("launch_ouster", default_value="False")
     declare_svo_path = DeclareLaunchArgument(
         "svo_path",
         default_value=TextSubstitution(text="live"),
@@ -43,16 +43,6 @@ def generate_launch_description():
             "use_composition": "False",
         }.items(),
     )
-
-    # Include traversability mapping if enabled
-    transversability_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_navigation, "launch", "traversability_gridmap.launch.py")
-        ),
-        condition=IfCondition(launch_transversability),
-    )
-
-    # Include ZED launch if enabled
     zed_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_navigation, "launch", "zed.launch.py")
@@ -61,23 +51,43 @@ def generate_launch_description():
         launch_arguments={"use_sim_time": use_sim_time, "svo_path": svo_path}.items(),
     )
 
+    # Include traversability mapping if enabled
+    if launch_zed and launch_ouster:
+        config = "all"
+    elif launch_zed:
+        config = "zed_only"
+    elif launch_ouster:
+        config = "ouster_only"
+    else:
+        print(
+            "Warning: Both ZED and Ouster launch are disabled; traversability mapping will not run."
+        )
+        config = "none"
+
+    traversability_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_navigation, "launch", "traversability_gridmap.launch.py")
+        ),
+        condition=IfCondition(launch_traversability),
+        launch_arguments={"profile": config, "use_sim_time": use_sim_time}.items(),
+    )
+
     # Include Ouster lidar launch if enabled
     ouster_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_navigation, "launch", "ouster_composable.launch.py")
         ),
         condition=IfCondition(launch_ouster),
-        # launch_arguments={"use_sim_time": use_sim_time}.items(),
     )
     return LaunchDescription(
         [
             declare_use_sim_time,
             declare_svo_path,
-            declare_transversability,
+            declare_traversability,
             declare_zed,
             declare_ouster,
             nav2_cmd,
-            transversability_cmd,
+            traversability_cmd,
             zed_cmd,
             ouster_cmd,
         ]
