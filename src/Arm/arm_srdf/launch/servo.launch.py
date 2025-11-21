89cb7569c58a96963ba53f7f2ea7e28b6a174b82
ConnorN <connor.needham2015@gmail.com>
2025-11-20 21:42:22 -0500

    chore: manual clean up

--- src/Arm/arm_srdf/launch/servo.launch.py
+++ src/Arm/arm_srdf/launch/servo.launch.py
@@ -1,15 +1,14 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_demo_launch
-from launch_param_builder import ParameterBuilder
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
+from launch.actions import GroupAction
+from launch.substitutions import PythonExpression
 import os
 import yaml
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import (
-    DeclareLaunchArgument,
     IncludeLaunchDescription,
 )
 from launch.conditions import IfCondition
@@ -17,12 +16,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-
-from srdfdom.srdf import SRDF
 
 from moveit_configs_utils.launch_utils import (
-    add_debuggable_node,
     DeclareBooleanLaunchArg,
 )
 
@@ -34,9 +29,7 @@ def load_yaml(package_name, file_path):
     try:
         with open(absolute_file_path, "r") as file:
             return yaml.safe_load(file)
-    except (
-        EnvironmentError
-    ):  # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:
         return None
 
 
@@ -51,26 +44,16 @@ def test_launch(moveit_config, launch_package_path=None):
      * robot_state_publisher
      * move_group
      * moveit_rviz
-     * warehouse_db (optional)
      * ros2_control_node + controller spawners
     """
     if launch_package_path == None:
         launch_package_path = moveit_config.package_path
 
     ld = LaunchDescription()
+
+    ld.add_action(DeclareBooleanLaunchArg("use_composition", default_value=True))
     ld.add_action(
-        DeclareBooleanLaunchArg(
-            "db",
-            default_value=False,
-            description="By default, we do not start a database (it can be large)",
-        )
-    )
-    ld.add_action(
-        DeclareBooleanLaunchArg(
-            "debug",
-            default_value=False,
-            description="By default, we are not in debug mode",
-        )
+        DeclareBooleanLaunchArg("use_intra_process_comms", default_value=True)
     )
     ld.add_action(DeclareBooleanLaunchArg("use_rviz", default_value=False))
     # If there are virtual joints, broadcast static tf by including virtual_joints launch
@@ -113,19 +96,6 @@ def test_launch(moveit_config, launch_package_path=None):
     )
 
     ld.add_action(
-        Node(
-            package="controller_manager",
-            executable="ros2_control_node",
-            parameters=[
-                str(moveit_config.package_path / "config/ros2_controllers.yaml"),
-            ],
-            remappings=[
-                ("/controller_manager/robot_description", "/robot_description"),
-            ],
-        )
-    )
-
-    ld.add_action(
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 str(launch_package_path / "launch/spawn_controllers.launch.py")
@@ -133,55 +103,89 @@ def test_launch(moveit_config, launch_package_path=None):
         )
     )
 
+    use_composition = LaunchConfiguration("use_composition")
+    use_intra_process_comms = LaunchConfiguration("use_intra_process_comms")
     servo_yaml = load_yaml("arm_srdf", "config/arm_config.yaml")
-    servo_params = {"moveit_servo": servo_yaml}
+    parameters = [
+        {
+            "moveit_servo": servo_yaml,
+            "use_intra_process_comms": use_intra_process_comms,
+        },
+        moveit_config.robot_description,
+        moveit_config.robot_description_semantic,
+        moveit_config.robot_description_kinematics,
+        str(moveit_config.package_path / "config/ros2_controllers.yaml"),
+    ]
 
-    # Launch as much as possible in components
+    load_nodes = GroupAction(
+        condition=IfCondition(PythonExpression(["not ", use_composition])),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="ros2_control_node",
+                output="screen",
+                respawn_delay=2.0,
+                parameters=parameters,
+                remappings=[
+                    ("/controller_manager/robot_description", "/robot_description")
+                ],
+            ),
+            Node(
+                package="moveit_servo",
+                executable="servo_node_main",
+                parameters=parameters,
+                output="screen",
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher::robot_state_publisher",
+                name="robot_state_publisher",
+                parameters=parameters,
+            ),
+        ],
+    )
+    container_name = "arm_container"
     container = ComposableNodeContainer(
-        name="moveit_servo_demo_container",
-        namespace="/",
+        namespace="",
         package="rclcpp_components",
         executable="component_container_mt",
+        name=container_name,
+        output="screen",
+        condition=IfCondition(use_composition),
+    )
+
+    # Group of actions to load composable nodes when composition is enabled
+    load_composable_nodes = LoadComposableNodes(
+        condition=IfCondition(use_composition),
+        target_container=container_name,
         composable_node_descriptions=[
-            # Example of launching Servo as a node component
-            # Assuming ROS2 intraprocess communications works well, this is a more efficient way.
-            # ComposableNode(
-            #     package="moveit_servo",
-            #     plugin="moveit_servo::ServoNode",
-            #     name="servo_server",
-            #     parameters=[
-            #         servo_params,
-            #         moveit_config.robot_description,
-            #         moveit_config.robot_description_semantic,
-            #         moveit_config.robot_description_kinematics,
-            #     ],
-            # ),
+            ComposableNode(
+                package="controller_manager",
+                plugin="controller_manager::ControllerManager",
+                name="controller_server",
+                parameters=parameters,
+                remappings=[
+                    ("/controller_manager/robot_description", "/robot_description")
+                ],
+            ),
+            ComposableNode(
+                package="moveit_servo",
+                plugin="moveit_servo::ServoNode",
+                name="servo_server",
+                parameters=parameters
+            ),
             ComposableNode(
                 package="robot_state_publisher",
                 plugin="robot_state_publisher::RobotStatePublisher",
                 name="robot_state_publisher",
-                parameters=[moveit_config.robot_description],
+                parameters=parameters,
             ),
         ],
-        output="screen",
-    )
-    # Launch a standalone Servo node.
-    # As opposed to a node component, this may be necessary (for example) if Servo is running on a different PC
-    servo_node = Node(
-        package="moveit_servo",
-        executable="servo_node_main",
-        parameters=[
-            servo_params,
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-            moveit_config.robot_description_kinematics,
-        ],
-        output="screen",
     )
 
+    ld.add_action(load_nodes)
+    ld.add_action(load_composable_nodes)
     ld.add_action(container)
-    ld.add_action(servo_node)
-
     return ld
 
 
