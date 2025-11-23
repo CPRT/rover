2920e4945cc15624913f8990dfdf12b4625d4fb2
Erik <43829793+ErikCald@users.noreply.github.com>
2025-11-22 21:49:51 -0500

    Add open loop drive option to talon drive controller (#28)
    * feat: open loop drive option
    
    * Format code and test
    
    ---------
    
    Co-authored-by: Jetson <softlead@curover.ca>

--- src/Teleop-Control/drive_cpp/src/WheelControl.cpp
+++ src/Teleop-Control/drive_cpp/src/WheelControl.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 
-WheelControl::WheelControl(std::string wheel_name, rclcpp::Node *node)
-    : node_(node), name_(wheel_name) {
+WheelControl::WheelControl(std::string wheel_name, rclcpp::Node *node,
+                           bool is_open_loop, double open_loop_scalar)
+    : node_(node), name_(wheel_name), is_open_loop_(is_open_loop),
+      open_loop_scalar_(open_loop_scalar) {
   if (wheel_name.find("Left") != std::string::npos ||
       wheel_name.find("left") != std::string::npos) {
     side_ = WheelSide::LEFT;
@@ -17,10 +19,27 @@ WheelControl::WheelControl(std::string wheel_name, rclcpp::Node *node)
         "Wheel name must contain either 'Left' or 'Right'");
   }
   pub_ = node->create_publisher<MotorControl>(wheel_name + "/set", 10);
-  control_.mode = MotorControl::VELOCITY;
+
+  if (is_open_loop_) {
+    control_.mode = MotorControl::PERCENT_OUTPUT;
+    RCLCPP_INFO(node->get_logger(),
+                "Wheel %s configured for open loop control with scalar %.3f",
+                wheel_name.c_str(), open_loop_scalar_);
+  } else {
+    control_.mode = MotorControl::VELOCITY;
+    RCLCPP_INFO(node->get_logger(),
+                "Wheel %s configured for closed loop velocity control",
+                wheel_name.c_str());
+  }
 }
 
-void WheelControl::setVelocity(double value) { control_.value = value; }
+void WheelControl::setVelocity(double value) {
+  if (is_open_loop_) {
+    control_.value = value * open_loop_scalar_;
+  } else {
+    control_.value = value;
+  }
+}
 
 void WheelControl::send() const { pub_->publish(control_); }
 
