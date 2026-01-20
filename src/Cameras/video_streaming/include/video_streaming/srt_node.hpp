8aff8132ee333930e73416e1ceb010dd1e576808
潤辰| Runchen Zhao <runchenzhao@cmail.carleton.ca>
2026-01-20 17:08:19 -0500

    framerate config (#46)
    * add framerate control and enhance parameter handling
    
    * PLI Back-off & Framerate Control  (#48)
    
    * feat: implement backoff mechanism for I-frame requests and enhance SrtStats message
    
    * refactor: standardize variable naming in BackoffState struct
    
    * feat: introduce configurable backoff parameters and enhance stability checks
    
    * feat: implement safe GStreamer element unref method and update SrtNode destructor
    
    * fix: improve packet loss handling and logging in SrtNode
    
    * fix: update interpipesrc configuration for improved pipeline performance
    
    * new message
    
    ---------
    
    Co-authored-by: Ju <bangmar@iclod.com>
    Co-authored-by: Jetson <softlead@curover.ca>

--- src/Cameras/video_streaming/include/video_streaming/srt_node.hpp
+++ src/Cameras/video_streaming/include/video_streaming/srt_node.hpp
@@ -3,6 +3,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/empty.hpp" // I-frame
 #include "std_msgs/msg/int32.hpp" // bitrate
+#include <chrono>                 // timers
 #include <gst/gst.h>
 #include <interfaces/msg/srt_stats.hpp>
 
@@ -11,7 +12,11 @@ namespace video_streaming {
 class SrtNode : public BaseVideoNode {
 public:
   explicit SrtNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
-  ~SrtNode() override = default;
+  ~SrtNode() override;
+
+  static constexpr int INITIAL_BACKOFF_MS = 200;
+  static constexpr int MAX_BACKOFF_MS = 5000;
+  static constexpr int BACKOFF_RESET_MS = 10000;
 
 protected:
   // (interpipesrc -> nvvidconv -> nvv4l2av1enc -> srtsink)
@@ -19,6 +24,7 @@ protected:
 
   GstElement *av1_encoder_ = nullptr;
   GstElement *srt_sink_ = nullptr;
+  GstElement *framerate_caps_ = nullptr;
 
   // bitrate
   rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr bitrate_sub_;
@@ -40,8 +46,23 @@ private:
 
   void publish_srt_stats();
 
+  void change_framerate(int new_fps);
+
   rcl_interfaces::msg::SetParametersResult
   on_parameter_change(const std::vector<rclcpp::Parameter> &parameters);
+
+  // Backoff mechanism to avoid excessive I-frame requests
+  void check_packet_loss_and_trigger(int64_t current_total_dropped);
+
+  struct BackoffState {
+    int current_delay_ms = INITIAL_BACKOFF_MS;
+    int max_delay_ms = MAX_BACKOFF_MS;
+    int reset_ms = BACKOFF_RESET_MS;
+
+    std::chrono::steady_clock::time_point last_trigger_time;
+    std::chrono::steady_clock::time_point last_loss_time;
+    int64_t last_total_dropped_pkts = 0;
+  } backoff_state_;
 };
 
 } // namespace video_streaming
