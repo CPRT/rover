5e2bd197d7fa3c15990d94be111087ce0b60e322
Connor Needham <129120300+ConnorNeed@users.noreply.github.com>
2026-02-09 17:23:27 +0000

    feat: add roboclaw node

---
+++ src/HW-Devices/ros_roboclaw/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+package_name = "ros_roboclaw"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="connor",
+    maintainer_email="connor.needham2015@gmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["roboclaw_node = ros_roboclaw.roboclaw_node:main"],
+    },
+)
