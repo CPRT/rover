a85e5e9724e1aaf0c1c64c6f6a6beb01cb260141
Jetson Nano1 <softlead@curover.ca>
2026-02-16 04:25:55 +0000

    WIP

--- src/Teleop-Control/joystick_control/include/arm.hpp
+++ src/Teleop-Control/joystick_control/include/arm.hpp
@@ -11,22 +11,22 @@
 class arm : public rclcpp::Node {
 public:
   arm();
-  void arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
 
 private:
+  void arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
+  void manual_arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
+  void ik_arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
   rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_pub_;
-  control_msgs::msg::JointJog joint_msg_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr ik_pub_;
+  enum ArmState { NONE = 0, MANUAL, IK };
+
+  ArmState current_state_;
 
   void declareParameters();
   void loadParameters();
+  bool initialized_ = false;
 
-  double kMaxBaseSpeed;
-  double kMaxWristRollSpeed;
-  double kMaxWristSpeed;
-  double kMaxAct1Speed;
-  double kMaxAct2Speed;
-  double kMaxElbowYawSpeed;
   int kThrottleAxis;
   int kBaseAxis;
   int kWristRoll;
@@ -35,6 +35,9 @@ private:
   int kAct1Axis;
   int kAct2Axis;
   int kElbowYaw;
+  int kDisableButton;
+  int kIkButton;
+  int kManualButton;
 };
 
 #endif
--- src/Teleop-Control/joystick_control/src/arm.cpp
+++ src/Teleop-Control/joystick_control/src/arm.cpp
@@ -4,36 +4,91 @@ arm::arm() : Node("arm_node") {
 
   declareParameters();
   loadParameters();
+  current_state_ = NONE;
 
   joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
       "/joy", 10, std::bind(&arm::arm_control, this, std::placeholders::_1));
   joint_pub_ = this->create_publisher<control_msgs::msg::JointJog>(
       "/servo_node/delta_joint_cmds", 10);
-  joint_msg_ = control_msgs::msg::JointJog();
-  joint_msg_.joint_names = {"Joint_1", "Joint_2", "Joint_3",
-                            "Joint_4", "Joint_5", "Joint_6"};
+  ik_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+      "/servo_node/delta_twist_cmds", 10);
 
   RCLCPP_INFO(this->get_logger(), "Arm controller started");
 }
 
 void arm::arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg) {
-  auto joint_msg_ = control_msgs::msg::JointJog();
+  auto &buttons = joystickMsg->buttons;
+  if (!initialized_) {
+    if (std::abs(joystickMsg->axes[kBaseAxis]) < 0.01 &&
+        std::abs(joystickMsg->axes[kAct1Axis]) < 0.01 &&
+        std::abs(joystickMsg->axes[kAct2Axis]) < 0.01) {
+      initialized_ = true;
+    }
+    return;
+  }
 
-  auto axes = joystickMsg->axes;
-  auto buttons = joystickMsg->buttons;
+  if (buttons[kDisableButton]) {
+    current_state_ = NONE;
+    RCLCPP_INFO(this->get_logger(), "Arm disabled");
+    return;
+  } else if (buttons[kIkButton]) {
+    current_state_ = IK;
+    RCLCPP_INFO(this->get_logger(), "Switched to IK control");
+  } else if (buttons[kManualButton]) {
+    current_state_ = MANUAL;
+    RCLCPP_INFO(this->get_logger(), "Switched to manual control");
+  }
 
-  joint_msg_.header = joystickMsg->header;
+  switch (current_state_) {
+  case MANUAL:
+    manual_arm_control(joystickMsg);
+    break;
+  case IK:
+    ik_arm_control(joystickMsg);
+    break;
+  default:
+    break;
+  }
+}
 
-  joint_msg_.velocities = {axes[kBaseAxis] * kMaxBaseSpeed,
-                           axes[kAct1Axis] * kMaxAct1Speed,
-                           axes[kAct2Axis] * kMaxAct2Speed,
-                           axes[kWristRoll] * kMaxWristRollSpeed,
-                           axes[kElbowYaw] * kMaxElbowYawSpeed,
-                           buttons[kWristYaw_positive] -
-                               buttons[kWristYaw_negative] * kMaxWristSpeed *
-                                   axes[kThrottleAxis]};
+void arm::manual_arm_control(
+    std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg) {
+  auto joint_msg = control_msgs::msg::JointJog();
+  joint_msg.joint_names = {"Joint_1", "Joint_2", "Joint_3",
+                           "Joint_4", "Joint_5", "Joint_6"};
 
-  joint_pub_->publish(joint_msg_);
+  auto &axes = joystickMsg->axes;
+  auto &buttons = joystickMsg->buttons;
+
+  joint_msg.header = joystickMsg->header;
+
+  joint_msg.velocities = {
+      axes[kBaseAxis],
+      axes[kAct1Axis],
+      axes[kAct2Axis],
+      axes[kWristRoll],
+      axes[kElbowYaw],
+      buttons[kWristYaw_positive] - buttons[kWristYaw_negative]};
+
+  joint_pub_->publish(joint_msg);
+}
+
+void arm::ik_arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg) {
+  auto twist_msg = geometry_msgs::msg::Twist();
+
+  auto &axes = joystickMsg->axes;
+  auto &buttons = joystickMsg->buttons;
+
+  twist_msg.linear.x = axes[kBaseAxis];
+  twist_msg.linear.y = axes[kAct1Axis];
+  twist_msg.linear.z = axes[kAct2Axis];
+  twist_msg.angular.x = axes[kWristRoll];
+  twist_msg.angular.y = axes[kElbowYaw];
+  twist_msg.angular.z = (static_cast<double>(buttons[kWristYaw_positive] -
+                                             buttons[kWristYaw_negative])) *
+                        axes[kThrottleAxis];
+
+  ik_pub_->publish(twist_msg);
 }
 
 void arm::declareParameters() {
@@ -45,12 +100,9 @@ void arm::declareParameters() {
   this->declare_parameter("act1_axis", 1);
   this->declare_parameter("act2_axis", 5);
   this->declare_parameter("elbow_yaw", 6);
-  this->declare_parameter("max_base_speed", 1.0);
-  this->declare_parameter("max_wrist_roll_speed", 1.0);
-  this->declare_parameter("max_wrist_speed", 1.0);
-  this->declare_parameter("max_act1_speed", 1.0);
-  this->declare_parameter("max_act2_speed", 1.0);
-  this->declare_parameter("max_elbow_yaw_speed", 1.0);
+  this->declare_parameter("ik_button", 10);
+  this->declare_parameter("manual_button", 11);
+  this->declare_parameter("disable_button", 9);
 }
 void arm::loadParameters() {
   this->get_parameter("throttle.axis", kThrottleAxis);
@@ -61,12 +113,9 @@ void arm::loadParameters() {
   this->get_parameter("act1_axis", kAct1Axis);
   this->get_parameter("act2_axis", kAct2Axis);
   this->get_parameter("elbow_yaw", kElbowYaw);
-  this->get_parameter("max_base_speed", kMaxBaseSpeed);
-  this->get_parameter("max_wrist_roll_speed", kMaxWristRollSpeed);
-  this->get_parameter("max_wrist_speed", kMaxWristSpeed);
-  this->get_parameter("max_act1_speed", kMaxAct1Speed);
-  this->get_parameter("max_act2_speed", kMaxAct2Speed);
-  this->get_parameter("max_elbow_yaw_speed", kMaxElbowYawSpeed);
+  this->get_parameter("ik_button", kIkButton);
+  this->get_parameter("manual_button", kManualButton);
+  this->get_parameter("disable_button", kDisableButton);
 }
 
 int main(int argc, char **argv) {
