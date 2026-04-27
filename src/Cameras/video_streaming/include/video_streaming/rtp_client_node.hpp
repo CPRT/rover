75574aa391b832670dacc8134d6d3cdeba413c23
ConnorN <connor.needham2015@gmail.com>
2026-04-27 23:28:02 +0000

    feat: visual arm control mode

--- src/Cameras/video_streaming/include/video_streaming/rtp_client_node.hpp
+++ src/Cameras/video_streaming/include/video_streaming/rtp_client_node.hpp
@@ -2,8 +2,11 @@
 
 #include "base_video_node.hpp"
 
+#include <geometry_msgs/msg/vector3.hpp>
 #include <gst/gst.h>
+#include <gstnvdsmeta.h>
 #include <interfaces/msg/rtp_stats.hpp>
+#include <nvdsmeta.h>
 #include <rcl_interfaces/msg/set_parameters_result.hpp>
 #include <rclcpp/node_options.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -27,6 +30,10 @@ private:
   int latency_ms_;
   rclcpp::TimerBase::SharedPtr rtp_stats_timer_;
   rclcpp::Publisher<interfaces::msg::RtpStats>::SharedPtr rtp_stats_pub_;
+  void draw_circle(NvDsBatchMeta *batch_meta, NvDsFrameMeta *frame_meta);
+  int circle_x_;
+  int circle_y_;
+  rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr dot_sub_;
 };
 
 } // namespace video_streaming
--- src/Teleop-Control/joystick_control/include/arm.hpp
+++ src/Teleop-Control/joystick_control/include/arm.hpp
@@ -1,8 +1,11 @@
 #ifndef ARM_HPP
 #define ARM_HPP
 
+#include "arm_control/MoveGroupClient.hpp"
 #include "control_msgs/msg/joint_jog.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "ros_phoenix/msg/motor_control.hpp"
 #include "sensor_msgs/msg/joy.hpp"
@@ -16,18 +19,24 @@ private:
   void arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
   void manual_arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
   void ik_arm_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
+  void ik_pose_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
   void endeffector_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
+  void clipboards_control(std::shared_ptr<sensor_msgs::msg::Joy> joystickMsg);
   bool moveit_servo_state(bool enable);
+  void clear_dot();
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
   rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_pub_;
   rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr ik_pub_;
   rclcpp::Publisher<ros_phoenix::msg::MotorControl>::SharedPtr eef_pub_;
-  enum ArmState { NONE = 0, MANUAL, IK };
+  rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr dot_pub_;
+  std::shared_ptr<arm_control::MoveGroupClient> moveit_client_;
+  enum ArmState { NONE = 0, MANUAL, IK, POS };
 
   ArmState current_state_;
 
   void declareParameters();
   void loadParameters();
+  geometry_msgs::msg::PoseStamped getPoseOfPtr();
   bool initialized_ = false;
 
   int kThrottleAxis;
@@ -41,8 +50,22 @@ private:
   int kDisableButton;
   int kIkButton;
   int kManualButton;
+  int kPositionButton;
   int kClawOpen;
   int kClawClose;
+  int kCamWidth;
+  int kCamHeight;
+  int kDotInc;
+  int kClipboard1SaveButton;
+  int kClipboard1ExecuteButton;
+  int kClipboard2SaveButton;
+  int kClipboard2ExecuteButton;
+
+  double targetPositionX;
+  double targetPositionY;
+  int clipboard1_pose_index_ = -1;
+  int clipboard2_pose_index_ = -1;
+  std::shared_ptr<sensor_msgs::msg::Joy> last_msg_;
 };
 
 #endif
