79257d366f776f2306e63c7ed94353ebdaea34c0
Jetson <softlead@curover.ca>
2026-04-28 22:35:39 +0000

    fix: replan 15 sec or invalid and actual use the smoother server

--- src/Nav/navigation/launch/nav2.launch.py
+++ src/Nav/navigation/launch/nav2.launch.py
@@ -128,7 +128,7 @@ def generate_launch_description():
 
     cmd_vel_topic_cmd = DeclareLaunchArgument(
         "cmd_vel_topic",
-        default_value="/cmd_vel",
+        default_value="/cmd_vel_tmp",
         description="cmd_vel topic (for remmaping)",
     )
 
