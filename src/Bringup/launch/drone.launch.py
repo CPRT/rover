fcde9c96e5d25218c7f1db1e7210eb58a67d2601
Nathaniel Hargrave <va7nfh@gmail.com>
2026-05-12 16:19:57 -0400

    accurate pi ip

--- src/Bringup/launch/drone.launch.py
+++ src/Bringup/launch/drone.launch.py
@@ -1,7 +1,7 @@
 import launch
 import launch_ros.actions
 
-PI_IP = "127.0.0.1"
+PI_IP = "192.168.0.25"
 
 
 def generate_launch_description():
@@ -12,7 +12,7 @@ def generate_launch_description():
                 executable="mavros_node",
                 name="mavros_node",
                 parameters=[
-                    {"fcu_url": f"udp://127.0.0.1:14550@{PI_IP}:14550"},
+                    {"fcu_url": f"udp://@{PI_IP}:14550"},
                     {"fcu_protocol": "v2.0"},
                     {"tgt_system": 1},
                     {"tgt_component": 1},
