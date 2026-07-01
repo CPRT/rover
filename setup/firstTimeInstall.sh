f7cffdf20697e474245fada438bee8541705b17b
ConnorN <connor.needham2015@gmail.com>
2026-07-01 21:07:21 +0000

    chore: fix emulated build

--- setup/firstTimeInstall.sh
+++ setup/firstTimeInstall.sh
@@ -10,6 +10,11 @@ else
 fi
 
 # install docker
+if !command -v docker >/dev/null 2>&1; then
+    curl -fsSL https://get.docker.com | sudo sh
+fi
+
+sudo usermod -aG docker $USER
 
 
 source ~/.bashrc
