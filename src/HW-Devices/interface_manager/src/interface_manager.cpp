7ac2fc1054473af2089cfa9b04e8faf21fbac8a0
Software Lead (Base Station) <softlead@curover.ca>
2026-07-12 21:18:04 +0000

    initial stats

--- src/HW-Devices/interface_manager/src/interface_manager.cpp
+++ src/HW-Devices/interface_manager/src/interface_manager.cpp
@@ -1,5 +1,10 @@
 #include "interface_manager.h"
 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 InterfaceManagerNode::InterfaceManagerNode(const std::string name,
                                            const rclcpp::NodeOptions &options)
     : rclcpp::Node(name, options) {
@@ -9,13 +14,34 @@ InterfaceManagerNode::InterfaceManagerNode(const std::string name,
       std::bind(&InterfaceManagerNode::timer_callback, this));
 
   this->can_status_pub_ =
-      this->create_publisher<interfaces::msg::CANStatus>("~/can_status", 10);
+      this->create_publisher<interfaces::msg::CANStatus>("/can_status", 10);
+
+  RCLCPP_INFO(get_logger(), "Interface Manager Node started");
 }
 
 void InterfaceManagerNode::timer_callback() {
   interfaces::msg::CANStatus msg;
-  msg.tx_errors = 6;
-  msg.rx_errors = 12;
+  int fd, len;
+  char buf[16];
+
+  fd = open("/sys/class/net/can0/statistics/tx_errors", O_RDONLY);
+  len = read(fd, buf, 16);
+  buf[len] = '\0';
+  msg.tx_errors = strtoul(buf, NULL, 0);
+  close(fd);
+
+  fd = open("/sys/class/net/can0/statistics/rx_errors", O_RDONLY);
+  len = read(fd, buf, 16);
+  buf[len] = '\0';
+  msg.rx_errors = strtoul(buf, NULL, 0);
+  close(fd);
+
+  fd = open("/sys/class/net/can0/operstate", O_RDONLY);
+  len = read(fd, buf, 16);
+  buf[len] = '\0';
+  msg.status = buf;
+  close(fd);
+
   if (this->can_status_pub_) {
     this->can_status_pub_->publish(msg);
   }
