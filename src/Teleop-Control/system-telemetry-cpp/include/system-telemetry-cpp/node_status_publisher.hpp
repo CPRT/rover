a78a568dc38ad7344af6e8ce6ef7def0ed4b5236
ConnorN <connor.needham2015@gmail.com>
2026-08-02 17:33:06 +0000

    feat: support topic fetching from webUI
    sqaush

--- src/Teleop-Control/system-telemetry-cpp/include/system-telemetry-cpp/node_status_publisher.hpp
+++ src/Teleop-Control/system-telemetry-cpp/include/system-telemetry-cpp/node_status_publisher.hpp
@@ -1,13 +1,12 @@
 #ifndef NODE_STATUS_PUBLISHER_HPP
 #define NODE_STATUS_PUBLISHER_HPP
 
+#include <interfaces/msg/node_list.hpp>
+#include <interfaces/srv/get_topics.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
 
 #include <string>
 #include <vector>
-
-#include "interfaces/msg/node_list.hpp"
 
 class NodeStatusPublisher : public rclcpp::Node {
 public:
@@ -16,9 +15,14 @@ public:
 private:
   void publish_nodes();
 
+  void
+  get_topics(const std::shared_ptr<interfaces::srv::GetTopics::Request> request,
+             std::shared_ptr<interfaces::srv::GetTopics::Response> response);
+
   double frequency_;
 
   rclcpp::Publisher<interfaces::msg::NodeList>::SharedPtr publisher_;
+  rclcpp::Service<interfaces::srv::GetTopics>::SharedPtr topics_service_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
--- src/Teleop-Control/system-telemetry-cpp/src/node_status_publisher.cpp
+++ src/Teleop-Control/system-telemetry-cpp/src/node_status_publisher.cpp
@@ -14,6 +14,11 @@ NodeStatusPublisher::NodeStatusPublisher()
   publisher_ =
       this->create_publisher<interfaces::msg::NodeList>("/system/nodes", qos);
 
+  topics_service_ = this->create_service<interfaces::srv::GetTopics>(
+      "/system/get_topics",
+      std::bind(&NodeStatusPublisher::get_topics, this, std::placeholders::_1,
+                std::placeholders::_2));
+
   const auto period = std::chrono::duration<double>(1.0 / frequency_);
   timer_ = this->create_wall_timer(
       std::chrono::duration_cast<std::chrono::nanoseconds>(period),
@@ -27,6 +32,21 @@ void NodeStatusPublisher::publish_nodes() {
   msg.nodes = this->get_node_names();
 
   publisher_->publish(msg);
+}
+
+void NodeStatusPublisher::get_topics(
+    const std::shared_ptr<interfaces::srv::GetTopics::Request> request,
+    std::shared_ptr<interfaces::srv::GetTopics::Response> response) {
+  (void)request;
+
+  const auto topics_and_types = this->get_topic_names_and_types();
+
+  for (const auto &[topic, types] : topics_and_types) {
+    for (const auto &type : types) {
+      response->topics.push_back(topic);
+      response->types.push_back(type);
+    }
+  }
 }
 
 int main(int argc, char **argv) {
